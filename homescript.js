84f7f0b516d96be3a13edd1f59c89ec4005538bc
Nab525 <muhdarfanabil@gmail.com>
2025-07-19 22:05:20 +0800

    Update

---
+++ homescript.js
@@ -0,0 +1,8 @@
+
+
+const myCarouselElement = document.querySelector('#carouselExampleIndicators')
+
+const carousel = new bootstrap.Carousel(myCarouselElement, {
+  interval: 2000,
+  touch: true
+})
